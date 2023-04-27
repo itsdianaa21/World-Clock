06f492c400de3b0a94f1b04325764e932fe56962
itsdianaa21 <dianamenendez17@gmail.com>
2023-04-27 14:16:08 -0500

    testing js

--- javascript/index.js
+++ javascript/index.js
@@ -30,17 +30,15 @@ function updateCity(event) {
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-      <div class="cities">
+      <div class="city">
         <div>
         <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
       </div>
-      <div>
-      <div class="time">${cityTime.format(
-        "h:mm:ss:SS"
-      )}<small>${cityTime.format(" A")}</small></div>
-          </div>
-          </div>
+      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
     `;
 }
 
