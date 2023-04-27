40452dcda73ad8ebc6197cfde5c089874ff7029d
itsdianaa21 <dianamenendez17@gmail.com>
2023-04-27 13:23:43 -0500

    organized updated js

--- javascript/index.js
+++ javascript/index.js
@@ -29,11 +29,17 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `<div class ="city"><div><h2>${cityName}</h2><div class="date">${cityTime.format(
-    "MMMM Do YYYY"
-  )}</div></div><div class="time">${cityTime.format(
-    "h:mm:ss"
-  )} <small>${cityTime.format("A")}</small></div></div>`;
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
 }
 updateTime();
 setInterval(updateTime, 1000);
