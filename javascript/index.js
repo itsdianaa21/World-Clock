bb1b14c3a21d868d63107f42ea2a47dff692c91b
itsdianaa21 <dianamenendez17@gmail.com>
2023-04-30 17:49:52 -0500

    added third city html and js

--- javascript/index.js
+++ javascript/index.js
@@ -23,6 +23,15 @@ function updateTime() {
     );
   }
 }
+let parisElement = document.querySelector("#paris");
+if (parisElement) {
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
